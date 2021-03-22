0e9ba866d312e7da88017d03a5211217e9782f14
Martin Sione <sionemartin@hotmail.com>
2021-03-22 09:43:11 -0300

    feat: header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>WIP: Amazon project</h1>
     </div>
   );
